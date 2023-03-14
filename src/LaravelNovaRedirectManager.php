b3e77b37bcb0e4db03e0a62ccc5f3af8db8de19b
Gilles Felix <felix@novius.fr>
2023-03-14 14:52:50 +0100

    Nova 4

--- src/LaravelNovaRedirectManager.php
+++ src/LaravelNovaRedirectManager.php
@@ -2,7 +2,11 @@
 
 namespace Novius\LaravelNovaRedirectManager;
 
+use Illuminate\Http\Request;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Tool;
+use Novius\LaravelNovaRedirectManager\Resources\Redirect;
 
 class LaravelNovaRedirectManager extends Tool
 {
@@ -14,5 +18,14 @@ class LaravelNovaRedirectManager extends Tool
     public function renderNavigation()
     {
         return view('laravel-nova-redirect-manager::navigation');
+    }
+
+    public function menu(Request $request): array
+    {
+        return [
+            MenuSection::make(trans('laravel-nova-redirect-manager::redirect.redirects'), [
+                MenuItem::resource(Redirect::class),
+            ])->icon('switch-horizontal'),
+        ];
     }
 }
